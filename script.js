71860d40ff285b628709adcde978acd468cf82c6
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2024-12-27 01:32:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -40,6 +40,18 @@ document.addEventListener('DOMContentLoaded', () => {
     updateCarousel();
   });
 
+
+document.addEventListener( 'DOMContentLoaded', function () {
+  new Splide( '#card-carousel', {
+		perPage    : 2,
+		breakpoints: {
+			640: {
+				perPage: 1,
+			},
+		},
+  } ).mount();
+} );
+    
   // Auto-slide every 5 seconds
   setInterval(() => {
     currentIndex = (currentIndex + 1) % totalImages;
